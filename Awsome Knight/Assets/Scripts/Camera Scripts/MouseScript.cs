67ed7392b098d37098e3724fef6157d2e200ae49
Harp1e <github@bplayer.waitrose.com>
2017-08-26 12:03:15 +0100

    L32-L33: Stone Runes Circle & Mouse Pointer

--- Awsome Knight/Assets/Scripts/Camera Scripts/MouseScript.cs
+++ Awsome Knight/Assets/Scripts/Camera Scripts/MouseScript.cs
@@ -5,19 +5,33 @@ using UnityEngine;
 public class MouseScript : MonoBehaviour 
 {
     public Texture2D cursorTexture;
-    //    public GameObject mousePoint;
+    public GameObject mousePoint;
 
     CursorMode mode = CursorMode.ForceSoftware;
     Vector2 hotSpot = Vector2.zero;
-    
-
-	void Start () 
-	{
-		
-	}
+    GameObject instantiatedMouse;
 	
 	void Update () 
 	{
         Cursor.SetCursor (cursorTexture, hotSpot, mode);
+        if (Input.GetMouseButtonDown(0))
+        {
+            Ray ray = Camera.main.ScreenPointToRay (Input.mousePosition);
+            RaycastHit hit;
+            if (Physics.Raycast (ray, out hit))
+            {
+                if (hit.collider is TerrainCollider)
+                {
+                    Vector3 temp = hit.point;
+                    temp.y = 0.25f;
+                    if (instantiatedMouse != null)
+                    {
+                        Destroy (instantiatedMouse);                      
+                    }
+                    instantiatedMouse = Instantiate (mousePoint) as GameObject;
+                    instantiatedMouse.transform.position = temp;
+                }
+            }
+        }
 	}
 }
--- Awsome Knight/Assets/Scripts/Player Scripts/PlayerMove.cs
+++ Awsome Knight/Assets/Scripts/Player Scripts/PlayerMove.cs
@@ -96,7 +96,7 @@ public class PlayerMove : MonoBehaviour {
                 Quaternion.LookRotation (target_Temp - transform.position),
                 15.0f * Time.deltaTime);
             player_Move = transform.forward * moveSpeed * Time.deltaTime;
-            if (Vector3.Distance (transform.position, target_Pos) <= 0.5f)
+            if (Vector3.Distance (transform.position, target_Pos) <= 0.1f)
             {
                 canMove = false;
             }
