910ec0fdd93f2778b5b2143bada02186878da034
Harp1e <github@bplayer.waitrose.com>
2017-08-26 16:23:00 +0100

    L37: Player Attack Script

---
+++ Awsome Knight/Assets/Scripts/Player Scripts/PlayerAttackEffects.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerAttackEffects : MonoBehaviour 
+{
+
+	void Start () 
+	{
+		
+	}
+
+    void GroundImpact ()
+    {
+
+    }
+
+    void Kick ()
+    {
+
+    }
+
+    void FireTornado ()
+    {
+
+    }
+
+    void FireShield ()
+    {
+
+    }
+
+    void Heal ()
+    {
+
+    }
+
+    void ThunderAttack ()
+    {
+
+    }
+}
--- Awsome Knight/Assets/Scripts/Player Scripts/PlayerMove.cs
+++ Awsome Knight/Assets/Scripts/Player Scripts/PlayerMove.cs
@@ -107,4 +107,28 @@ public class PlayerMove : MonoBehaviour {
             anim.SetFloat ("Walk", 0f);
         }
     }
+
+    public bool FinishedMovement
+    {
+        get
+        {
+            return finished_Movement;
+        }
+        set
+        {
+            finished_Movement = value;
+        }
+    }
+
+    public Vector3 TargetPosition
+    {
+        get
+        {
+            return target_Pos;
+        }
+        set
+        {
+            target_Pos = value;
+        }
+    }
 }
