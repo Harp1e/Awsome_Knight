0f6db7969eddfea9ad64592ba1f121e8fe658ee9
Harp1e <github@bplayer.waitrose.com>
2017-08-27 19:48:43 +0100

    L46: Enemy Boss

--- Awsome Knight/Assets/Scripts/Enemy Scripts/EnemyHealth.cs
+++ Awsome Knight/Assets/Scripts/Enemy Scripts/EnemyHealth.cs
@@ -7,27 +7,11 @@ public class EnemyHealth : MonoBehaviour
 {
     public float health = 100f;
 
-    Image health_Img;
-
-    void Awake ()
-    {
-        if (tag == "Boss")
-        {
-            health_Img = GameObject.Find ("Health Foreground Boss").GetComponent<Image>();
-        }
-        else
-        {
-            health_Img = GameObject.Find ("Health Foreground").GetComponent<Image> ();
-        }
-    }
+    public Image health_Img;
 
     public void TakeDamage (float amount)
     {
         health -= amount;
         health_Img.fillAmount = health / 100f;
-        if (health <= 0)
-        {
-
-        }
     }
 }
